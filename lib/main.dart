35c1fca57d40ad97c89014928e1e7b7f53aacf0b
Mizuno Hikaru <machikaru@MizunonoMBP.lan>
2022-02-21 12:17:21 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,221 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutter/services.dart';
+import 'package:soundpool/soundpool.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OK Google君',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final textController = TextEditingController();
+  double volume = 20;
+  bool active = true;
+
+  @override
+  void initState() {
+    super.initState();
+    textController.addListener(() => setState(() {}));
+    // _initSounds();
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  bool checkPlatform() {
+    if (Platform.isIOS) {
+      return true;
+    } else if (Platform.isAndroid) {
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _initSounds(
+    String file,
+  ) async {
+    Soundpool? _pool = Soundpool.fromOptions(
+        options: SoundpoolOptions(streamType: StreamType.notification));
+
+    // ファイルの読み込み
+    int soundId = await rootBundle.load(file).then((ByteData soundData) {
+      return _pool.load(soundData);
+    });
+
+    int streamId = await _pool.play(soundId);
+    _pool.setVolume(soundId: soundId, streamId: streamId, volume: volume);
+  }
+
+  FlutterTts flutterTts = FlutterTts();
+  String inputText = "";
+
+  Future speak(String text) async {
+    await flutterTts.setLanguage("ja");
+    await flutterTts.setPitch(1);
+    await flutterTts.setVolume(10);
+    await flutterTts.setSpeechRate(0.5);
+    await flutterTts.speak(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const leadingStyle1 = TextStyle(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue);
+    const leadingStyle2 =
+        TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OK グーグル君",
+            style: TextStyle(color: active ? Colors.white : Colors.black)),
+        centerTitle: true,
+        backgroundColor: active ? Colors.blue : Colors.black,
+        elevation: 0.0,
+      ),
+      // extendBodyBehindAppBar: false,
+      body: active
+          ? Center(
+              child: ListView(
+                children: [
+                  const ListTile(
+                    leading: Text("準備１", style: leadingStyle1),
+                    title: Text(
+                      "質問を考える",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                  const ListTile(
+                    leading: Text("準備２", style: leadingStyle1),
+                    title: Text(
+                      "音を最大にする",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Text("準備３", style: leadingStyle1),
+                    title: CupertinoTextField(
+                      controller: textController,
+                      maxLines: 5,
+                      padding: EdgeInsets.all(16),
+                      placeholder: "読み上げるテキストを入力",
+                      placeholderStyle: TextStyle(color: Colors.black38),
+                      suffix: CupertinoButton(
+                          child: Icon(CupertinoIcons.clear),
+                          onPressed: () {
+                            textController.clear();
+                            inputText = textController.text;
+                          }),
+                      onChanged: (input) {
+                        inputText = input;
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Text("手順１", style: leadingStyle2),
+                    title: checkPlatform()
+                        ? const Text(
+                            "「OK グーグル」と言ってみよう",
+                            style: TextStyle(fontSize: 30),
+                          )
+                        : const Text(
+                            "「OK グーグル」と言ってみよう",
+                            style: TextStyle(fontSize: 30),
+                          ),
+                  ),
+                  ListTile(
+                    leading: const Text("手順２", style: leadingStyle2),
+                    title: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _initSounds("assets/pushUp.mp3");
+                          });
+                        },
+                        child: const Text("ピコん！ (音)")),
+                  ),
+                  const ListTile(
+                    leading: Text("手順３", style: leadingStyle2),
+                    title: Text(
+                      "質問する",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Text("手順４", style: leadingStyle2),
+                    title: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _initSounds("assets/pushDown.mp3");
+                          });
+                          Future.delayed(const Duration(seconds: 2), () {
+                            speak(textController.text);
+                          });
+                        },
+                        child: const Text("入力したテキストを読ませる")),
+                  ),
+                ],
+              ),
+            )
+          : Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage('assets/bg.png'),
+                fit: BoxFit.fill,
+              )),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(
+                    height: 180,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(13.0),
+                    child: Text(
+                      inputText,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: active ? Colors.pink : Colors.white.withOpacity(0.0),
+          onPressed: () {
+            setState(() {
+              active = !active;
+            });
+          },
+          child: Icon(
+              active ? Icons.change_circle : Icons.change_circle_outlined,
+              size: 40)),
+    );
+  }
+}
